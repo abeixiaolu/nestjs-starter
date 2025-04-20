4096de16dc2750bc48d4b7302a8ce3dffabf2d0e
abei <abeixiaolu@gmail.com>
2025-04-20 16:22:41 +0800

    在 CoreModule 中引入并注册 TransformResponseInterceptor

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
-
   getHello() {
-    const env = this.configService.get<string>('environment');
-    return env;
+    return 'Hello World';
   }
 }

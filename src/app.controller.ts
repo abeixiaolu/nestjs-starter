f5461294052f8d6baf8d5e5f55df167d15c80f3b
abei <abeixiaolu@gmail.com>
2025-04-21 23:22:13 +0800

    引入 Helmet 中间件和全局验证管道

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './create-user.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello() {
     return this.appService.getHello();
+  }
+
+  @Post()
+  createUser(@Body() payload: CreateUserDto) {
+    console.log('payload: ', payload);
+    return payload;
   }
 }

e37244b8810ec1e57ffaf6ad64f2d4677f7bd29b
abei <abeixiaolu@gmail.com>
2025-04-22 17:36:59 +0800

    添加 LoggerService 和 LoggerMiddleware，更新 AppService 以使用日志记录功能

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { LoggerService } from './core/logger/logger.service';
 @Injectable()
 export class AppService {
+  constructor(private readonly logger: LoggerService) {}
   getHello() {
+    this.logger.warn('Hello World from AppService', 'AppService');
     return 'Hello World';
   }
 }

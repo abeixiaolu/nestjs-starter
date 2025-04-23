ce8f622250bd5ffbc60f88e62b66d74def31f6aa
abei <abeixiaolu@gmail.com>
2025-04-23 19:57:18 +0800

    添加数据库服务和 Prisma 支持，更新 AppService 以连接数据库，并在 CoreModule 中注册 DatabaseService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { LoggerService } from './core/logger/logger.service';
+import { DatabaseService } from './database/database.service';
 @Injectable()
 export class AppService {
-  constructor(private readonly logger: LoggerService) {}
-  getHello() {
-    this.logger.warn('Hello World from AppService', 'AppService');
-    return 'Hello World';
+  constructor(
+    private readonly logger: LoggerService,
+    private readonly database: DatabaseService,
+  ) {}
+  async getHello() {
+    this.logger.log('Connecting to database...');
+    const users = await this.database.user.findMany();
+    return users;
   }
 }
